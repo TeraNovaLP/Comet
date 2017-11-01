c0ffd553dbb07066a35be3438f7e9b17dc8a04e5
TeraNovaLP <TeraNovaLPs@gmail.com>
2017-11-01 20:04:46 +0100

    Add base Source code.

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import {app, BrowserWindow, Menu} from 'electron';
+import {shell} from 'electron';
+
+var mainForm;
+
+app.on('ready', () => {
+    mainForm = new BrowserWindow({
+        width: 1200,
+        height: 700,
+        minWidth: 1200,
+        minHeight: 700,
+        center: true,
+        title: "PlayPerium Comet"
+    })
+
+    mainForm.loadURL(`file://${__dirname}/main.html`);
+    setFormMenu();
+})
+
+app.once('window-all-closed', app.quit);
+
+function setFormMenu() {
+    var formMenu = [
+    {
+        label: "Comet",
+        submenu: [
+            {
+                label: "Developer Tools",
+                click() {mainForm.toggleDevTools();}
+            },
+            {
+                label: "Exit",
+                click() {app.quit();}
+            }
+        ]
+    },
+    {
+        label: "Help",
+        submenu: [
+            {
+                label: "License",
+                click() {shell.openExternal('https://github.com/PlayPerium/Comet/blob/master/LICENSE');}
+            }
+        ]
+    },
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(formMenu));
+}
