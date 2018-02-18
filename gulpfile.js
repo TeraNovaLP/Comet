17bef2e15687efd2133d58fa53b23573df3de50b
TeraNovaLP <TeraNovaLPs@gmail.com>
2018-02-18 22:04:48 +0100

    Made some optimization to the Gulp Tasks

--- gulpfile.js
+++ gulpfile.js
@@ -6,35 +6,48 @@ const del = require("del");
 const childProcess = require("child_process");
 const electron = require("electron");
 const builder = require("electron-builder");
+const runSeq = require("run-sequence");
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("start", ["compile", "copy"], function() {
+gulp.task("start", function(callback) {
+    runSeq("compile", "copy", "run", callback);
+})
+
+gulp.task("run", function(callback) {
     childProcess.spawn(electron, ["app/main.js"], {
         stdio: "inherit"
     })
     .on("close", function () {
-        process.exit();
+        callback();
     })
 })
 
-gulp.task("build", ["compile", "copy"], function() {
+gulp.task("runBuild", function(callback) {
     builder.build();
+    callback();
 })
 
-gulp.task("cbuild", ["clean", "compile", "copy"], function() {
-    builder.build();
+gulp.task("build", function(callback) {
+    runSeq("compile", "copy", "runBuild", callback)
 })
 
-gulp.task("compile", function() {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("app"));
+gulp.task("cbuild", function(callback) {
+    runSeq("clean", "compile", "copy", "runBuild", callback);
 })
 
-gulp.task("copy", function() {
-    return gulp.src(["src/**/*", "!src/**/*.ts"]).pipe(gulp.dest("app"));
+gulp.task("compile", function(callback) {
+    tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("app"));
+    callback();
 })
 
-gulp.task("clean", function() {
+gulp.task("copy", function(callback) {
+    gulp.src(["src/**/*", "!src/**/*.ts"]).pipe(gulp.dest("app"));
+    callback();
+})
+
+gulp.task("clean", function(callback) {
     del("app/**/*");
     del("dist/**/*");
+    callback();
 })
