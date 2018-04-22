57152644e18fa125aa2db9648136b1a462aae1ad
TeraNovaLP <TeraNovaLPs@gmail.com>
2018-04-22 22:44:11 +0200

    Reverted Gulp changes and updated Dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
@@ -6,48 +6,36 @@ const del = require("del");
 const childProcess = require("child_process");
 const electron = require("electron");
 const builder = require("electron-builder");
-const runSeq = require("run-sequence");
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("start", function(callback) {
-    runSeq("compile", "copy", "run", callback);
-})
-
-gulp.task("run", function(callback) {
+gulp.task("start", ["compile", "copy"], function() {
     childProcess.spawn(electron, ["app/main.js"], {
         stdio: "inherit"
     })
-    .on("close", function () {
-        callback();
-    })
+        .on("close", function () {
+            process.exit();
+        });
 })
 
-gulp.task("runBuild", function(callback) {
+gulp.task("build", ["compile", "copy"], function() {
     builder.build();
-    callback();
 })
 
-gulp.task("build", function(callback) {
-    runSeq("compile", "copy", "runBuild", callback)
+gulp.task("cbuild", ["clean", "compile", "copy"], function() {
+    builder.build();
 })
 
-gulp.task("cbuild", function(callback) {
-    runSeq("clean", "compile", "copy", "runBuild", callback);
+gulp.task("compile", function() {
+    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("app"));
 })
 
-gulp.task("compile", function(callback) {
-    tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("app"));
-    callback();
+gulp.task("copy", function() {
+    return gulp.src(["src/**/*", "!src/**/*.ts"]).pipe(gulp.dest("app"));
 })
 
-gulp.task("copy", function(callback) {
-    gulp.src(["src/**/*", "!src/**/*.ts"]).pipe(gulp.dest("app"));
-    callback();
-})
 
-gulp.task("clean", function(callback) {
+gulp.task("clean", function() {
     del("app/**/*");
     del("dist/**/*");
-    callback();
 })
